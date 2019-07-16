5df97670b8c1d1632548d1ae16d57da54dfac5ca
HAEUN JEONG <jhe3263@gmail.com>
2019-07-16 19:17:14 -0400

    Create Main form

---
+++ COMP123-S2019-Lab10-301004579/MainForm.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace COMP123_S2019_Lab10_301004579
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Control = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Control
+            // 
+            this.Control.AutoSize = true;
+            this.Control.Location = new System.Drawing.Point(66, 51);
+            this.Control.Name = "Control";
+            this.Control.Size = new System.Drawing.Size(392, 34);
+            this.Control.TabIndex = 0;
+            this.Control.Text = "Welcome to Main Form!";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(19F, 33F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(1857, 1055);
+            this.Controls.Add(this.Control);
+            this.Font = new System.Drawing.Font("Gulim", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Main Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Control;
+    }
+}
+
